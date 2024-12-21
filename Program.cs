8fa780308a704a35d4ca961ebe021fb02d08825e
Vitor <vitor.suzarte@ecalc.com.br>
2024-12-20 22:53:30 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Nome
+        //Identificado Númerico
+        //Apelido
+        //Custo Hora
+        //Tipo
+        //Velocidade
+
+        Console.WriteLine("===================================================");
+        Console.WriteLine("Cadastro de Máquina");
+        Console.WriteLine("===================================================");
+        Console.WriteLine();
+
+        Console.Write("Digite o nome da máquina: ");
+        string nomeMaquina = Console.ReadLine();
+
+        Console.WriteLine("===================================================");
+
+
+        Console.WriteLine($"Nome: {nomeMaquina}");
+    }
+}
